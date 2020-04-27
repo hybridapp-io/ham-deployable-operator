4858dfac5f52e8656a1e75940b521c2d05293a23
Kuan Feng <fengkuan@gmail.com>
2020-04-27 16:51:27 -0400

    initial check in
    Signed-off-by: Kuan Feng <fengkuan@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,120 @@
-# ham-deployable-operator
+# Deployable Operator
+
+[![Build](http://prow.purple-chesterfield.com/badge.svg?jobs=images-ham-deployable-operator-amd64-postsubmit)](http://prow.purple-chesterfield.com/?job=images-ham-deployable-operator-amd64-postsubmit)
+[![GoDoc](https://godoc.org/github.com/IBM/hybriddeploayble-operator?status.svg)](https://godoc.org/github.com/IBM/ham-deployable-operator)
+[![Go Report Card](https://goreportcard.com/badge/github.com/IBM/ham-deployable-operator)](https://goreportcard.com/report/github.com/IBM/ham-deployable-operator)
+[![Code Coverage](https://codecov.io/gh/IBM/ham-deployable-operator/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/IBM/ham-deployable-operator?branch=master)
+[![License](https://img.shields.io/:license-apache-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+[![Container](https://quay.io/repository/multicloudlab/hybriddeployable-operator/status)](https://quay.io/repository/multicloudlab/hybriddeployable-operator?tab=tags)
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*
+
+- [What is the Deployable Operator](#what-is-the-deployable-operator)
+- [Community, discussion, contribution, and support](#community-discussion-contribution-and-support)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Quick Start](#quick-start)
+    - [Clone Deployable Operator Repository](#clone-hybriddeployable-operator-repository)
+    - [Build Deployable Operator](#build-hybriddeployable-operator)
+    - [Install Deployable Operator](#install-hybriddeployable-operator)
+    - [Uninstall Deployable Operator](#uninstall-hybriddeployable-operator)
+  - [Troubleshooting](#troubleshooting)
+- [References](#references)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+## What is the Deployable Operator
+
+The hybridDeployable resource is introduced to handle deployable components running on non-kubernetes platform(s). This operator is intended to work as part of collection of operators for the HybridApplication.  See [References](#hybridApplication-references) for additional information.
+
+## Community, discussion, contribution, and support
+
+Check the [CONTRIBUTING Doc](CONTRIBUTING.md) for how to contribute to the repo.
+
+------
+
+## Getting Started
+
+### Prerequisites
+
+- git v2.18+
+- Go v1.13.4+
+- operator-sdk v0.15.1
+- Kubernetes v1.14+
+- kubectl v1.14+
+
+Check the [Development Doc](docs/development.md) for how to contribute to the repo.
+
+### Quick Start
+
+#### Clone Deployable Operator Repository
+
+```shell
+$ mkdir -p "$GOPATH"/src/github.com/hybridapp-io
+$ cd "$GOPATH"/src/github.com/hybridapp-io
+$ git clone https://github.com/hybridapp-io/ham-deployable-operator.git
+$ cd "$GOPATH"/src/github.com/hybridapp-io/ham-deployable-operator
+```
+
+#### Build Deployable Operator
+
+Build the ham-deployable-operator and push it to a registry.  Modify the example below to reference a container reposistory you have access to.
+
+```shell
+$ operator-sdk build quay.io/<user>/ham-deployable-operator:v0.1.0
+$ sed -i 's|REPLACE_IMAGE|quay.io/johndoe/ham-deployable-operator:v0.1.0|g' deploy/operator.yaml
+$ docker push quay.io/johndoe/ham-deployable-operator:v0.1.0
+```
+
+#### Install Deployable Operator
+
+Register the CRD.
+
+```shell
+$ kubectl create -f deploy/crds/app.cp4mcm.ibm.com_hybriddeployables_crd.yaml
+```
+
+Setup RBAC and deploy.
+
+```shell
+$ kubectl create -f deploy/service_account.yaml
+$ kubectl create -f deploy/role.yaml
+$ kubectl create -f deploy/role_binding.yaml
+$ kubectl create -f deploy/operator.yaml
+```
+
+Verify ham-deployable-operator is up and running.
+
+```shell
+$ kubectl get deployment
+NAME                        READY   UP-TO-DATE   AVAILABLE   AGE
+ham-deployable-operator   1/1     1            1           2m20s
+```
+
+Create the sample CR.
+
+```shell
+$ kubectl create -f deploy/crds/app.cp4mcm.ibm.com_hybriddeployables_cr.yaml
+NAME                        READY   UP-TO-DATE   AVAILABLE   AGE
+ham-deployable-operator   1/1     1            1           2m20s
+$ kubectl get hybriddeployables
+NAME     AGE
+simple   11s
+```
+
+#### Uninstall Deployable Operator
+
+Remove all resources created.
+
+```shell
+$ kubectl delete -f deploy
+$ kubectl delete -f deploy/crds
+```
+
+### Troubleshooting
+
+Please refer to [Troubleshooting documentation](docs/trouble_shooting.md) for further info.
+
+## References
---
+++ pkg/controller/deployable/hybriddeployable_placement.go
@@ -0,0 +1,286 @@
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deployable
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	clusterv1alpha1 "k8s.io/cluster-registry/pkg/apis/clusterregistry/v1alpha1"
+	"k8s.io/klog"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	placementv1 "github.com/open-cluster-management/multicloud-operators-placementrule/pkg/apis/apps/v1"
+	placementutils "github.com/open-cluster-management/multicloud-operators-placementrule/pkg/utils"
+
+	corev1alpha1 "github.com/hybridapp-io/ham-deployable-operator/pkg/apis/core/v1alpha1"
+	hdplutils "github.com/hybridapp-io/ham-deployable-operator/pkg/utils"
+)
+
+// Top priority: placementRef, ignore others
+// Next priority: clusterNames, ignore selector
+// Bottomline: Use label selector
+func (r *ReconcileHybridDeployable) getDeployersByPlacement(instance *corev1alpha1.Deployable) ([]*corev1alpha1.Deployer, error) {
+	if instance == nil || instance.Spec.Placement == nil {
+		return nil, nil
+	}
+
+	var deployers []*corev1alpha1.Deployer
+
+	var err error
+
+	if instance.Spec.Placement.PlacementRef != nil {
+		deployers, err = r.getDeployersByPlacementReference(instance)
+		return deployers, err
+	}
+
+	if instance.Spec.Placement.Deployers != nil {
+		for _, dplyref := range instance.Spec.Placement.Deployers {
+			deployer := &corev1alpha1.Deployer{}
+
+			err = r.Get(context.TODO(), types.NamespacedName{Name: dplyref.Name, Namespace: dplyref.Namespace}, deployer)
+			if err != nil {
+				klog.V(packageDetailLogLevel).Info("Trying to obtain object from deployers list, but got error: ", err)
+				continue
+			}
+
+			annotations := deployer.GetAnnotations()
+			if annotations == nil {
+				annotations = make(map[string]string)
+			}
+
+			deployer.SetAnnotations(annotations)
+
+			hdplutils.SetInClusterDeployer(deployer)
+			deployers = append(deployers, deployer)
+		}
+
+		return deployers, err
+	}
+
+	if instance.Spec.Placement.DeployerLabels != nil {
+		deployers, err = r.getDeployersByLabelSelector(instance.Spec.Placement.DeployerLabels)
+		return deployers, err
+	}
+
+	return nil, nil
+}
+
+func (r *ReconcileHybridDeployable) getDeployersByLabelSelector(deployerLabels *metav1.LabelSelector) ([]*corev1alpha1.Deployer, error) {
+	clSelector, err := placementutils.ConvertLabels(deployerLabels)
+	if err != nil {
+		return nil, err
+	}
+
+	klog.V(packageDetailLogLevel).Info("Using Cluster LabelSelector ", clSelector)
+
+	dplylist := &corev1alpha1.DeployerList{}
+
+	err = r.List(context.TODO(), dplylist, &client.ListOptions{LabelSelector: clSelector})
+
+	if err != nil && !errors.IsNotFound(err) {
+		klog.Error("Listing clusters and found error: ", err)
+		return nil, err
+	}
+
+	klog.V(packageDetailLogLevel).Info("listed deployers:", dplylist.Items)
+
+	var deployers []*corev1alpha1.Deployer
+
+	for _, dply := range dplylist.Items {
+		deployer := dply.DeepCopy()
+
+		annotations := deployer.GetAnnotations()
+		if annotations == nil {
+			annotations = make(map[string]string)
+		}
+
+		deployer.SetAnnotations(annotations)
+
+		hdplutils.SetInClusterDeployer(deployer)
+		deployers = append(deployers, deployer)
+	}
+
+	return deployers, err
+}
+
+func (r *ReconcileHybridDeployable) getDeployersByPlacementReference(instance *corev1alpha1.Deployable) ([]*corev1alpha1.Deployer, error) {
+	var deployers []*corev1alpha1.Deployer
+
+	clustermap, err := r.getClusterMapByPlacementRule(instance)
+
+	if err != nil {
+		klog.Error("Failed find target namespaces with error: ", err)
+		return nil, err
+	}
+
+	klog.V(packageDetailLogLevel).Info("Find clusters for hybrid deployable:", clustermap)
+
+	for _, cl := range clustermap {
+		dset := &corev1alpha1.DeployerSet{}
+		key := types.NamespacedName{
+			Namespace: cl.Namespace,
+			Name:      cl.Name,
+		}
+		deployer := &corev1alpha1.Deployer{}
+		deployer.Name = cl.Name
+		deployer.Namespace = cl.Namespace
+		deployer.Spec.Type = corev1alpha1.DefaultDeployerType
+
+		err = r.Get(context.TODO(), key, dset)
+		klog.V(packageDetailLogLevel).Info("Got Deployerset for cluster ", cl.GetNamespace(), "/", cl.GetName(), " with err:", err, " result: ", dset)
+
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				continue
+			}
+		} else {
+			if len(dset.Spec.Deployers) > 0 {
+				dset.Spec.Deployers[0].Spec.DeepCopyInto(&deployer.Spec)
+
+				if dset.Spec.DefaultDeployer != "" {
+					for _, dply := range dset.Spec.Deployers {
+						if dply.Key == dset.Spec.DefaultDeployer {
+							dply.Spec.DeepCopyInto(&deployer.Spec)
+							break
+						}
+					}
+				}
+				klog.V(packageDetailLogLevel).Info("Copyied deployer info:", deployer)
+			}
+		}
+
+		klog.V(packageDetailLogLevel).Info("Adding deployer: ", deployer)
+
+		annotations := deployer.GetAnnotations()
+		if annotations == nil {
+			annotations = make(map[string]string)
+		}
+
+		deployer.SetAnnotations(annotations)
+		hdplutils.SetRemoteDeployer(deployer)
+
+		deployers = append(deployers, deployer)
+	}
+
+	klog.V(packageDetailLogLevel).Info("Deploying to deployers", deployers)
+
+	return deployers, nil
+}
+
+func (r *ReconcileHybridDeployable) getChildren(request types.NamespacedName) (map[schema.GroupVersionResource]gvrChildrenMap, error) {
+	children := make(map[schema.GroupVersionResource]gvrChildrenMap)
+
+	nameLabel := map[string]string{
+		corev1alpha1.HostingHybridDeployable: request.Name,
+	}
+
+	for gvr := range r.activeGVRMap {
+		objlist, err := r.dynamicClient.Resource(gvr).List(metav1.ListOptions{LabelSelector: labels.Set(nameLabel).String()})
+		if err != nil {
+			return nil, err
+		}
+
+		klog.V(packageDetailLogLevel).Info("Processing active gvr", gvr, "and got list:", objlist.Items)
+
+		gvkchildren := make(map[types.NamespacedName]metav1.Object)
+
+		for _, obj := range objlist.Items {
+			annotations := obj.GetAnnotations()
+			if annotations == nil {
+				continue
+			}
+
+			if host, ok := annotations[corev1alpha1.HostingHybridDeployable]; ok {
+				if host == request.String() {
+					key := r.genObjectIdentifier(&obj)
+
+					if _, ok := gvkchildren[key]; ok {
+						klog.Info("Deleting redundant deployed object", obj.GetNamespace(), "/", obj.GetName())
+
+						deletePolicy := metav1.DeletePropagationBackground
+
+						_ = r.dynamicClient.Resource(gvr).Namespace(obj.GetNamespace()).Delete(obj.GetName(), &metav1.DeleteOptions{PropagationPolicy: &deletePolicy})
+					} else {
+						gvkchildren[key] = obj.DeepCopy()
+					}
+
+					klog.V(packageDetailLogLevel).Info("Adding gvk child with key:", key)
+				}
+			}
+		}
+
+		children[gvr] = gvkchildren
+	}
+
+	return children, nil
+}
+
+func (r *ReconcileHybridDeployable) getClusterMapByPlacementRule(instance *corev1alpha1.Deployable) (map[string]*clusterv1alpha1.Cluster, error) {
+	if instance == nil || instance.Spec.Placement == nil || instance.Spec.Placement.PlacementRef == nil {
+		return nil, nil
+	}
+
+	pref := instance.Spec.Placement.PlacementRef
+
+	// only default to mcm placementrule, and only support mcm placementrule now
+	if len(pref.Kind) > 0 && pref.Kind != "PlacementRule" || len(pref.APIVersion) > 0 && pref.APIVersion != "app.ibm.com/v1alpha1" {
+		klog.Warning("Unsupported placement reference:", pref)
+
+		return nil, nil
+	}
+
+	clustermap := make(map[string]*clusterv1alpha1.Cluster)
+
+	klog.V(packageDetailLogLevel).Info("Referencing existing PlacementRule:", pref)
+
+	// get placementpolicy resource
+	pp := &placementv1.PlacementRule{}
+	pkey := types.NamespacedName{
+		Name:      pref.Name,
+		Namespace: pref.Namespace,
+	}
+
+	if pref.Namespace == "" {
+		pkey.Namespace = instance.Namespace
+	}
+
+	err := r.Get(context.TODO(), pkey, pp)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			klog.Warning("Failed to locate placement reference", instance.Spec.Placement.PlacementRef)
+
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	klog.V(packageDetailLogLevel).Info("Preparing cluster namespaces from ", pp)
+
+	for _, decision := range pp.Status.Decisions {
+		cluster := &clusterv1alpha1.Cluster{}
+		cluster.Name = decision.ClusterName
+		cluster.Namespace = decision.ClusterNamespace
+		clustermap[decision.ClusterName] = cluster
+	}
+
+	return clustermap, nil
+}
